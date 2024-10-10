cdbb72482638b189ec92c55f3035a6476f74596f
jonas pauli <jonaspauli089@gmail.com>
2024-10-11 00:37:01 +0200

    temp hardcode

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 mod api;
 mod ntfy;
 mod types;
-use std::time::Duration;
+use std::{collections::HashMap, time::Duration};
 
 use api::nonkyc::NonKycClient;
 use ntfy::NftyClient;
 use std::env;
 use types::nonkyc::{MarketData, MarketWrapper, TickerData};
 // Luckycoin loop
-async fn nonkyc(pairs: &[&str], sleep_duration: u64) {
+async fn nonkyc(pairs: &[&str], balances: HashMap<&str, f64>, sleep_duration: u64) {
     loop {
         let nonkyc_client = NonKycClient;
         let ntfy_client = NftyClient {
@@ -25,13 +25,26 @@ async fn nonkyc(pairs: &[&str], sleep_duration: u64) {
                         MarketWrapper::MarketData(market_data) => {
                             message =
                                 format!("{}: {}", &pair, market_data.simple_price().to_string());
+                            let price = market_data
+                                .simple_price()
+                                .to_string()
+                                .parse::<f64>()
+                                .unwrap();
+
+                            println!("{}, {}$", message, price * balances.get(pair).unwrap());
                         }
                         MarketWrapper::TickerData(ticker_data) => {
                             message =
                                 format!("{}: {}", &pair, ticker_data.simple_price().to_string());
+                            let price = ticker_data
+                                .simple_price()
+                                .to_string()
+                                .parse::<f64>()
+                                .unwrap();
+                            println!("{}, {}$", message, price * balances.get(pair).unwrap());
                         }
                     }
-                    let _ = ntfy_client.submit(message, &pair).await;
+                    // let _ = ntfy_client.submit(message, &pair).await;
                 }
                 Err(_) => {}
             };
@@ -42,10 +55,34 @@ async fn nonkyc(pairs: &[&str], sleep_duration: u64) {
 
 #[tokio::main]
 async fn main() {
-    let pairs = ["LKY_USDT", "BEL_USDT", "TDC_USDT"];
+    let mut balances: HashMap<&str, f64> = HashMap::new();
+    let pairs = [
+        "LKY_USDT",
+        "BEL_USDT",
+        "TDC_USDT",
+        "HTN_USDT",
+        "HOLE_USDT",
+        "FREN_USDT",
+        "SLOPY_USDT",
+        "NYANTE_USDT",
+        "PHIL_USDT",
+        "SMCN_USDT",
+        "WOW_USDT",
+    ];
+    balances.insert("LKY_USDT", 302.0);
+    balances.insert("BEL_USDT", 1300.0);
+    balances.insert("TDC_USDT", 1250.0);
+    balances.insert("HTN_USDT", 105272.0);
+    balances.insert("HOLE_USDT", 3985538229171.0);
+    balances.insert("FREN_USDT", 7923281.0);
+    balances.insert("SLOPY_USDT", 67482057975.0);
+    balances.insert("NYANTE_USDT", 403502933732.0);
+    balances.insert("PHIL_USDT", 14228012387.0);
+    balances.insert("SMCN_USDT", 12463.0);
+    balances.insert("WOW_USDT", 38.0);
     let sleep_duration: u64 = env::var("SLEEP_DURATION")
         .unwrap_or("300".to_string())
         .parse()
         .unwrap();
-    nonkyc(&pairs, sleep_duration).await;
+    nonkyc(&pairs, balances, sleep_duration).await;
 }
