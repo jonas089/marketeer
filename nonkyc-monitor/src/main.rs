e7077f5142d68099c1f394a5c004e7a713c03bdd
jonas pauli <jonaspauli089@gmail.com>
2024-10-23 15:18:23 +0200

    add mina and zec

--- nonkyc-monitor/src/main.rs
+++ nonkyc-monitor/src/main.rs
@@ -2,14 +2,22 @@ use std::time::Duration;
 
 use balances::get_non_zero_balances;
 use colored::*;
-use marketeer::api::nonkyc::{
-    types::{Balances, MarketWrapper},
-    NonKycClient,
+use marketeer::api::{
+    coingecko::CoinGeckoClient,
+    nonkyc::{
+        types::{Balances, MarketWrapper},
+        NonKycClient,
+    },
 };
 use tokio::time::sleep;
 mod balances;
+
+const MINA: f64 = 5571.0;
+const ZEC: f64 = 17.5;
+
 #[tokio::main]
 async fn main() {
+    let gecko = CoinGeckoClient;
     let client = NonKycClient;
     loop {
         let balances: Balances = get_non_zero_balances().await;
@@ -52,6 +60,33 @@ async fn main() {
                 None => {}
             }
         }
+        // render some custom coingecko data
+        let prices = gecko.get_token_prices("mina-protocol,zcash", "usd").await;
+        let mina_price = prices.get("mina-protocol").unwrap().get("usd").unwrap();
+        let zec_price = prices.get("zcash").unwrap().get("usd").unwrap();
+        println!(
+            "[{}] \n Amount: {}, Price: {}$, Value: {}$",
+            MINA.to_string().red().bold(),
+            MINA.to_string(),
+            mina_price.to_string().yellow(),
+            (MINA * mina_price).to_string().green()
+        );
+        println!(
+            "[{}] \n Amount: {}, Price: {}$, Value: {}$",
+            ZEC.to_string().red().bold(),
+            ZEC.to_string(),
+            zec_price.to_string().yellow(),
+            (ZEC * zec_price).to_string().green()
+        );
         sleep(Duration::from_millis(3000)).await;
     }
+}
+
+#[tokio::test]
+async fn test_get_mina_zec() {
+    let gecko = CoinGeckoClient;
+    let prices = gecko.get_token_prices("mina-protocol,zcash", "usd").await;
+    let mina_price = prices.get("mina-protocol").unwrap().get("usd").unwrap();
+    let zec_price = prices.get("zcash").unwrap().get("usd").unwrap();
+    println!("Mina: {:?}, Zec: {:?}", &mina_price, zec_price);
 }
